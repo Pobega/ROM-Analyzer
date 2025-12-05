6a38b9b83fa0d7aa9539d0d0a203209a0bd3092f
Michael Pobega <pobega@gmail.com>
2025-12-05 02:35:29 -0500

    Refactor CHD and ZIP processing
    Refactor CHD and ZIP processing to return the inner data rather than
    process it. This allows us to centralize the process_rom_data logic.
    
    This is in an effort to make less functions public.

--- src/archive/zip.rs
+++ src/archive/zip.rs
@@ -2,20 +2,17 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
-use log::{debug, error};
+use log::debug;
 use zip::ZipArchive;
 
-use crate::RomAnalysisResult;
 use crate::SUPPORTED_ROM_EXTENSIONS;
 use crate::error::RomAnalyzerError;
 
 pub fn process_zip_file(
     file: File,
     original_filename: &str,
-    process_rom_data_fn: &dyn Fn(Vec<u8>, &str) -> Result<RomAnalysisResult, Box<dyn Error>>,
-) -> Result<RomAnalysisResult, Box<dyn Error>> {
+) -> Result<(Vec<u8>, String), Box<dyn Error>> {
     let mut archive = ZipArchive::new(file)?;
-    let mut found_rom = false;
 
     debug!("[+] Analyzing ZIP archive: {}", original_filename);
 
@@ -34,31 +31,15 @@ pub fn process_zip_file(
 
         if is_supported_rom {
             debug!("[+] Found supported ROM in zip: {}", entry_name);
-            found_rom = true;
             let mut data = Vec::new();
             file_in_zip.read_to_end(&mut data)?;
 
-            match process_rom_data_fn(data, &entry_name) {
-                Ok(analysis) => {
-                    return Ok(analysis);
-                }
-                Err(e) => {
-                    error!("[!] Analysis failed for {}: {}", entry_name, e);
-                }
-            }
+            return Ok((data, entry_name));
         }
     }
 
-    if found_rom {
-        Err(Box::new(RomAnalyzerError::new(&format!(
-            "No valid ROM header found in supported files within {}",
-            original_filename
-        )))
-        .into())
-    } else {
-        return Err(Box::new(RomAnalyzerError::new(&format!(
-            "No supported ROM files found within the zip archive: {}",
-            original_filename
-        ))));
-    }
+    Err(Box::new(RomAnalyzerError::new(&format!(
+        "No supported ROM files found within the zip archive: {}",
+        original_filename
+    ))))
 }
